59fe5582e73cd373461bc2788ae928343a99febf
CatMoe <70209532+CatMoe@users.noreply.github.com>
2022-03-22 09:55:28 +0800

    revert build

--- build-default.bat
+++ build-default.bat
@@ -21,40 +21,40 @@ cmake --build . --config Release -j %NUMBER_OF_PROCESSORS%
 cmake --build . --config Release --target install
 popd
 
-mkdir win-JNI-x64
-pushd win-JNI-x64
-cmake -T "v142,host=x64" -A "x64" ^
-  -DCMAKE_INSTALL_PREFIX=install ^
-  -DCMAKE_BUILD_TYPE=Release -DOCR_OUTPUT="JNI" ..
-cmake --build . --config Release -j %NUMBER_OF_PROCESSORS%
-cmake --build . --config Release --target install
-popd
+@REM mkdir win-JNI-x64
+@REM pushd win-JNI-x64
+@REM cmake -T "v142,host=x64" -A "x64" ^
+@REM   -DCMAKE_INSTALL_PREFIX=install ^
+@REM   -DCMAKE_BUILD_TYPE=Release -DOCR_OUTPUT="JNI" ..
+@REM cmake --build . --config Release -j %NUMBER_OF_PROCESSORS%
+@REM cmake --build . --config Release --target install
+@REM popd
 
-mkdir win-JNI-Win32
-pushd win-JNI-Win32
-cmake -T "v142,host=x64" -A "Win32" ^
-  -DCMAKE_INSTALL_PREFIX=install ^
-  -DCMAKE_BUILD_TYPE=Release -DOCR_OUTPUT="JNI" ..
-cmake --build . --config Release -j %NUMBER_OF_PROCESSORS%
-cmake --build . --config Release --target install
-popd
+@REM mkdir win-JNI-Win32
+@REM pushd win-JNI-Win32
+@REM cmake -T "v142,host=x64" -A "Win32" ^
+@REM   -DCMAKE_INSTALL_PREFIX=install ^
+@REM   -DCMAKE_BUILD_TYPE=Release -DOCR_OUTPUT="JNI" ..
+@REM cmake --build . --config Release -j %NUMBER_OF_PROCESSORS%
+@REM cmake --build . --config Release --target install
+@REM popd
 
-mkdir win-CLIB-x64
-pushd win-CLIB-x64
-cmake -T "v142,host=x64" -A "x64" ^
-  -DCMAKE_INSTALL_PREFIX=install ^
-  -DCMAKE_BUILD_TYPE=Release -DOCR_OUTPUT="CLIB" ..
-cmake --build . --config Release -j %NUMBER_OF_PROCESSORS%
-cmake --build . --config Release --target install
-popd
+@REM mkdir win-CLIB-x64
+@REM pushd win-CLIB-x64
+@REM cmake -T "v142,host=x64" -A "x64" ^
+@REM   -DCMAKE_INSTALL_PREFIX=install ^
+@REM   -DCMAKE_BUILD_TYPE=Release -DOCR_OUTPUT="CLIB" ..
+@REM cmake --build . --config Release -j %NUMBER_OF_PROCESSORS%
+@REM cmake --build . --config Release --target install
+@REM popd
 
-mkdir win-CLIB-Win32
-pushd win-CLIB-Win32
-cmake -T "v142,host=x64" -A "Win32" ^
-  -DCMAKE_INSTALL_PREFIX=install ^
-  -DCMAKE_BUILD_TYPE=Release -DOCR_OUTPUT="CLIB" ..
-cmake --build . --config Release -j %NUMBER_OF_PROCESSORS%
-cmake --build . --config Release --target install
-popd
+@REM mkdir win-CLIB-Win32
+@REM pushd win-CLIB-Win32
+@REM cmake -T "v142,host=x64" -A "Win32" ^
+@REM   -DCMAKE_INSTALL_PREFIX=install ^
+@REM   -DCMAKE_BUILD_TYPE=Release -DOCR_OUTPUT="CLIB" ..
+@REM cmake --build . --config Release -j %NUMBER_OF_PROCESSORS%
+@REM cmake --build . --config Release --target install
+@REM popd
 
 @ENDLOCAL
